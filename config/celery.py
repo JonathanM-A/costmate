fe5f43c2b86e7b6308749d099ad5ed2af217ada4
JonathanM-A <kamajthomas@gmail.com>
2025-10-20 14:43:07 +0000

    renaming

--- config/celery.py
+++ config/celery.py
@@ -4,7 +4,7 @@ from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.base")
 
-app = Celery("costmate")
+app = Celery("costnav")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
