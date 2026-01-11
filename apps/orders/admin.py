1ec7b28739dae17834fcd4f5b7590d91008d6ba2
JonathanM-A <kamajthomas@gmail.com>
2026-01-11 09:14:13 +0000

    Setting up Orders and Subscriptions Admin

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -1,3 +1,72 @@
 from django.contrib import admin
+from .models import Order, OrderRecipe
 
-# Register your models here.
+
+class RecipeInline(admin.TabularInline):
+    model = OrderRecipe
+    extra = 1
+    readonly_fields = ("line_value",)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "order_no",
+        "customer",
+        "total_value",
+        "tax_amount",
+        "status",
+        "delivery_date",
+        "created_at",
+    )
+    search_fields = ("order_no", "customer__name")
+    list_filter = ("status", "delivery_date", "created_at")
+    inlines = [RecipeInline]
+    ordering = ("-created_at",)
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+        "total_value",
+        "tax_amount",
+        "profit",
+        "profit_percentage",
+        "order_no",
+    )
+
+    fieldsets = (
+        (
+            "Order Information",
+            {
+                "fields": ("customer", "delivery_date", "status"),
+            },
+        ),
+        (
+            "Financial Information",
+            {
+                "fields": ("total_value", "tax_amount", "profit", "profit_percentage"),
+            },
+        ),
+        (
+            "System Information",
+            {
+                "fields": ("order_no", "created_by", "created_at", "updated_at"),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+
+
+@admin.register(OrderRecipe)
+class OrderRecipeAdmin(admin.ModelAdmin):
+    list_display = ("order__order_no", "recipe", "quantity", "line_value")
+    search_fields = ("order__order_no", "recipe__name")
+    readonly_fields = ("line_value",)
+
+    fieldsets = (
+        (
+            "Order Recipe Information",
+            {
+                "fields": ("order", "recipe", "quantity", "line_value"),
+            },
+        ),
+    )
--- apps/subscriptions/admin.py
+++ apps/subscriptions/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Subscription
 
-# Register your models here.
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "tier",
+        "current_sub_start",
+        "current_sub_end",
+        "is_active",
+    )
+    search_fields = (
+        "user",
+        "subscripition_code",
+    )
+    list_filter = ("current_sub_start", "current_sub_end", "is_active")
+    ordering = ("current_sub_start",)
+    readonly_fields = ("current_sub_start", "current_sub_end", "is_active")
+
+    fieldsets = (
+        (
+            "Subscription Information",
+            {
+                "fields": (
+                    "user",
+                    "tier",
+                    "current_sub_start",
+                    "current_sub_end",
+                    "is_active",
+                ),
+            },
+        ),
+    )
