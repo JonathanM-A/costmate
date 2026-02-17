65cf1b393048de1d59e1aaf3533f9581cee5dcfa
JonathanM-A <kamajthomas@gmail.com>
2026-02-17 16:30:22 +0000

    Adding logo to Business Model

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework import permissions
 import debug_toolbar
 from drf_yasg.views import get_schema_view
@@ -79,3 +81,7 @@ urlpatterns = [
     ),
     path("sentry-debug/", trigger_error),
 ]
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
