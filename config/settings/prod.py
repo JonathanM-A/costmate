f237000eb8fb8b2bfc3a1da71f87b3fb255c4baf
JonathanM-A <kamajthomas@gmail.com>
2026-02-28 01:39:55 +0000

    Reorder middleware to prioritize CORS handling in production settings

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -99,9 +99,9 @@ LOCAL_APPS = [
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
